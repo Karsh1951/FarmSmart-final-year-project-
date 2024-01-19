e100fd4df80370252db10d0a0cfc6ac93efaff4d
Karsh1951 <giftonkarsh9851@gmail.com>
2024-01-19 04:08:26 -0800

    created a settings page and connected it to the views.py

--- farm_smart/core/views.py
+++ farm_smart/core/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html')  
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'setting.html')
+
+
+
 
 def signup(request):
 
@@ -27,13 +36,15 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
 
                 #create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -56,7 +67,13 @@ def signin(request):
             messages.info(request, 'Credentials Invalid')
             return redirect('signin')
 
-        return render(request,'signin.html')
+    
+    return render(request,'signin.html') 
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
     
 
 
